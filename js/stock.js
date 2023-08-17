d562649c0d4e63e71ab4c66e0ea1efe63c19a882
Francisco Oliva <franoliva845@gmail.com>
2023-08-17 08:58:30 -0300

    Entrega Final 1.3.2

--- js/stock.js
+++ js/stock.js
@@ -6,7 +6,7 @@ const arrayPerfumes =
             price: 21990,
             cantidad: 1,
             description: "La combinación del cuero, ananá y madera de sándalo crea una fragancia sofisticada y elegante. El ananá aporta frescura, el cuero profundidad y el sándalo una nota equilibrada, perfecta para quienes buscan una fragancia única.",
-            foto: "/img/men.png"
+            foto: "./img/men.png"
         },
         {
             id: 2,
@@ -14,7 +14,7 @@ const arrayPerfumes =
             price: 24990,
             cantidad: 1,
             description: "Combina la frescura de la menta junto con la vitalidad del limón y la tranquilidad de la lavanda, creando una fragancia fresca y energizante. Perfecta para utilizar algo fresco en toda ocación.",
-            foto: "/img/ocean.png"
+            foto: "./img/ocean.png"
         },
         {
             id: 3,
@@ -22,7 +22,7 @@ const arrayPerfumes =
             price: 24990,
             cantidad: 1,
             description: "Combina la dulzura del coco con la acidez cítrica de la mandarina, complementada por la profundidad terrosa del vétiver. El resultado es una fragancia tropical y sofisticada que evoca las imágenes de las puestas del sol en la playa.",
-            foto: "/img/sunset.png"
+            foto: "./img/sunset.png"
         },
         {
             id: 4,
@@ -30,7 +30,7 @@ const arrayPerfumes =
             price: 29990,
             cantidad: 1,
             description: "Combina la suave dulzura de la vainilla, el picante del cardamomo y la calidez del ámbar, creando una fragancia exótica y envolvente. Perfecta para quienes buscan una fragancia distinta y exótica.",
-            foto: "/img/night.png"
+            foto: "./img/night.png"
         },
         {
             id: 5,
@@ -38,7 +38,7 @@ const arrayPerfumes =
             price: 34990,
             cantidad: 1,
             description: "Combina las notas de tabaco y ron con la suavidad del iris y la calidez de la haba tonka. Su cautivador aroma evoca las noches misteriosas y elegantes. Una fragancia perfecta para despertar la pasión y crear una noche inolvidable",
-            foto: "/img/gold.png",
-            foto2: "/img/goldCopy.png"
+            foto: "./img/gold.png",
+            foto2: "./img/goldCopy.png"
         },
     ]
